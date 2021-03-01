442eac2ea6fb9b8f1aebc5cb60e21bf7b720690f
kevin.ngouleu <kevin.ngouleu@epita.fr>
2021-03-01 01:23:15 +0100

    Menu
    Premier envoi de Kevin du menu

---
+++ Orientation Menu/Assets/startDATA.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startDATA : MonoBehaviour
+{
+    public GameObject croix_party2;
+    public GameObject croix_party;
+    public GameObject createparty;
+    public GameObject joinparty;
+
+    public void joinwin()
+    {
+        joinparty.SetActive(true);
+    }
+
+    public void Startwin()
+    {
+        createparty.SetActive(true);
+    }
+    public void croixfunction()
+    {
+        createparty.SetActive(false);
+    }
+    public void croixfunction2()
+    {
+        joinparty.SetActive(false);
+    }
+
+
+    /*// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+
+}
