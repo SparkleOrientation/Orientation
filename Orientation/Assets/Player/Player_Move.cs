d5ff88123dc411b058b783200ecd6be2c1784360
camil.ziane <camil.ziane@epita.fr>
2021-03-01 10:48:06 +0100

    Mouvement du Personnage
    Camera qui suit le personnage

---
+++ Orientation/Assets/Player/Player_Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    public float speed = 5f;
+    Rigidbody2D rb;
+    private Vector2 dir;
+   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        dir.x = Input.GetAxisRaw("Horizontal") ;
+        dir.y = Input.GetAxis("Vertical");
+        rb.MovePosition(rb.position + dir * speed * Time.fixedDeltaTime);
+    }
+}
