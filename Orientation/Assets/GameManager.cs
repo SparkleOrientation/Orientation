c4dc42d9cc25224d6cb1ed360f7f46e64f5ae156
camil.ziane <camil.ziane@epita.fr>
2021-04-27 10:41:32 +0200

    Player Name correction

--- Orientation/Assets/GameManager.cs
+++ Orientation/Assets/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,11 +10,18 @@ public class GameManager : Photon.MonoBehaviour
     public GameObject PlayerPrefab;
     public GameObject GameCanvas;
     public GameObject SceneCamera;
+    public Text PingText;
 
     private void Awake()
     {
         GameCanvas.SetActive(true);
     }
+
+    private void Update()
+    {
+        PingText.text = "Ping " + PhotonNetwork.GetPing();
+    }
+
     public void SpawnPlayer()
     {
         PhotonNetwork.Instantiate(PlayerPrefab.name, new Vector2(-76, 38), Quaternion.identity, 0);
